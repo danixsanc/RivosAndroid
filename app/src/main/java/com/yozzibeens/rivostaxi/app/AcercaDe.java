ff3caf00f2a28931cf4308dd41ce67e171dda7ac
daniel sanchez <d.ani.loko@hotmail.com>
2016-02-02 11:50:49 -0700

    commit
    commit

---
+++ app/src/main/java/com/yozzibeens/rivostaxi/app/AcercaDe.java
@@ -0,0 +1,47 @@
+package com.yozzibeens.rivostaxi.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.yozzibeens.rivostaxi.R;
+
+
+public class AcercaDe extends AppCompatActivity {
+
+    ImageButton back_button;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acerca_de);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
+
+
+
+
+
+
